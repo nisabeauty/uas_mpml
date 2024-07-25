5fc96b8e573b3ff49073aac48f4baad2c1557950
nisabeauty <nisabeautyutami@gmail.com>
2024-07-25 11:37:30 +0700

    uas mpml

---
+++ app.py
@@ -0,0 +1,18 @@
+import numpy as np
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+# Load the trained model
+model = joblib.load("model.pkl")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    input_features = np.array([data['features']])
+    prediction = model.predict(input_features)
+    return jsonify({"prediction": int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
